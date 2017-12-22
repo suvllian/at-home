1f9f4632ea6eb442a454af971816df51addb3f50
宋青松 <suvlliansong@163.com>
2017-12-23 00:57:10 +0800

    'feat:up'

--- at-home/pages/whole-house/index.js
+++ at-home/pages/whole-house/index.js
@@ -1,13 +1,30 @@
 Page({
   data: {
-    areaArray: ['100㎡—135㎡', '136㎡—180㎡', '100㎡以上'],
-    areaIndex: 0,
+    areaList: ['99㎡以下', '100㎡—135㎡', '136㎡—180㎡', '180㎡以上'],
+    priceList: [100, 200, 300, 400],
+    selectedIndex: 0,
+    coupons: 0,
+    memberScale: 0.1,
+    discountMoney: 0,
     time: '00:00',
     date: (new Date()).getFullYear() + '-' + (new Date()).getMonth() + '-' + (new Date()).getDate()
   },
-  bindPickerChange: function (e) {
+  onLoad () {
+    const { priceList, memberScale, coupons, selectedIndex } = this.data
+    const discountMoney = (priceList[selectedIndex] * memberScale + coupons).toFixed(2)
+    
     this.setData({
-      areaIndex: e.detail.value
+      discountMoney
+    })
+  },
+  bindPickerChange: function (e) {
+    const selectedIndex = e.detail.value
+    const { priceList, memberScale, coupons } = this.data
+    const discountMoney = (priceList[selectedIndex] * memberScale + coupons).toFixed(2)
+
+    this.setData({
+      selectedIndex,
+      discountMoney
     })
   },
   bindDateChange: function (e) {
