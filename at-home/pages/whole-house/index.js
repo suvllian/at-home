7d645d6b930140181e5575b457661ff13c080036
曦舒 <qingsong.sqs@alibaba-inc.com>
2017-12-23 11:49:02 +0800

    feat: fix

--- at-home/pages/whole-house/index.js
+++ at-home/pages/whole-house/index.js
@@ -7,6 +7,8 @@ Page({
     memberScale: 0.1,
     discountMoney: 0,
     time: '00:00',
+    multiArray: [['上午', '下午'], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]],
+    multiIndex: [0, 8, 9],
     date: (new Date()).getFullYear() + '-' + (new Date()).getMonth() + '-' + (new Date()).getDate()
   },
   onLoad () {
@@ -15,6 +17,12 @@ Page({
     
     this.setData({
       discountMoney
+    })
+  },
+  // 修改预约时间
+  bindMultiPickerChange: function (e) {
+    this.setData({
+      multiIndex: e.detail.value
     })
   },
   bindPickerChange: function (e) {
