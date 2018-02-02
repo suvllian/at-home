9628b4c8198ec34e67293b0cdf651115a76b067b
宋青松 <suvlliansong@163.com>
2018-02-03 00:03:02 +0800

    'feat:up'

---
+++ node-end/models/db-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  devMysql: {
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'zaihu',
+    port: 3306
+  },
+  mysql: {
+    host: '172.19.0.3',
+    user: 'suvllian',
+    password: 'HELLOsuvvlian890',
+    database: 'zaihu',
+    port: 3306
+  }
+}
---
+++ wechat-front/config.js
@@ -0,0 +1,20 @@
+// API路径
+// export const APIROOT = 'http://127.0.0.1:8080'
+export const APIROOT = 'https://zaihu.zhangguanzhang.com'
+// 预约时间选择值
+export const TIMEPICKERVALUE = [['上午', '下午'], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]]
+// 服务电话
+export const SERVICENUMBER = '029-81111852'
+// 小程序APPID
+export const APPID = 'wx8c9b59640486010c'
+// 微信商户KEY
+export const PAYKEY = 'zaihuiwangluoyouxiangongsi199801'
+// 电器中最大和最小数量
+export const MAXCOUNT = 10
+export const MINCOUNT = 0
+// 用户信息KEY
+export const STORAGEKEY = 'userInformation'
+// 券信息KEY
+export const COUPONSTORAGEKEY = 'couponInformation'
+
+
